8d18fcfd5d6c8510ab026db6120ab767fb98554c
Karan Chauhan <karanchouhan211@gmail.com>
2024-05-20 15:16:07 +0200

    catch all segments

---
+++ app/products/[[...slug]]/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+interface Props {
+	params: { slug: string[] };
+}
+export default function ProductPage({ params: { slug } }: Props) {
+	return <div>ProductPage {slug}</div>;
+}
