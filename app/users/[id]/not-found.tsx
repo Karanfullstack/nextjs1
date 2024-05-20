3967cd729848355cc52577b127cb7ed45ea70cb3
Karan Chauhan <karanchouhan211@gmail.com>
2024-05-20 20:56:09 +0200

    handling not found errors

---
+++ app/users/[id]/not-found.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function UserNotFoundPage() {
+  return (
+    <div>Given Id User Not Found</div>
+  )
+}
--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,8 +1,10 @@
+import { notFound } from "next/navigation";
 import React from "react";
 
 interface Props {
 	params: { id: number };
 }
-export default function UserDetial({params:{id}}:Props) {
+export default function UserDetial({ params: { id } }: Props) {
+	if (id > 11) notFound();
 	return <div>UserDetial {id}</div>;
 }
