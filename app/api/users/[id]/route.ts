7aab750258c33a6f06636003a99beb65b0505796
Karan Chauhan <karanchouhan211@gmail.com>
2024-05-24 19:17:50 +0200

    updating user

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -3,7 +3,6 @@ import schema from "../schema";
 import prisma from "@/prisma/client";
 // TODO request:NextRequest - IF REMOVE THIS DATA IS CACHED OTHERWISE NO
 
-
 interface Props {
 	params: { id: number };
 }
@@ -18,19 +17,29 @@ export async function GET(request: NextRequest, { params: { id } }: Props) {
 
 export async function PUT(request: NextRequest, { params: { id } }: Props) {
 	const body = await request.json();
+
 	const validation = schema.safeParse(body);
 	if (!validation.success)
 		return NextResponse.json(
 			{ error: validation.error.errors },
 			{ status: 400 }
 		);
-	if (id > 12)
+
+	const user = await prisma.user.findUnique({
+		where: { id: Number(id) },
+	});
+
+	if (!user)
 		return NextResponse.json({ error: "Invalid User ID" }, { status: 404 });
-	return NextResponse.json({ id: 1, name: body.name }, { status: 200 });
+	const updateUser = await prisma.user.update({
+		where: { id: Number(id) },
+		data: { name: body.name, email: body.email },
+	});
+	return NextResponse.json(updateUser, { status: 200 });
 }
 
 export function DELETE(request: NextRequest, { params: { id } }: Props) {
 	if (id > 12)
 		return NextResponse.json({ error: "User not found" }, { status: 404 });
-	return NextResponse.json({ message: "User deleted successfully" });
+	else return NextResponse.json({ message: "User deleted successfully" });
 }
