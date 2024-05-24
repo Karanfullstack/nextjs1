c0aa6bf859c12dbc22f90da70159d87458b698e4
Karan Chauhan <karanchouhan211@gmail.com>
2024-05-24 20:43:36 +0200

    showing image

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" data-theme="cupcake">
-			<body className={inter.className}>
+			<body className={inter.className} suppressHydrationWarning={true}>
 				<Navbar />
 				<main>{children}</main>
 			</body>
--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,14 +1,30 @@
 "use client";
-import React from "react";
-import { CldUploadWidget } from "next-cloudinary";
+import React, { useState } from "react";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+
+interface CloudnaryProps {
+	public_id: string;
+}
 export default function UploadPage() {
+	const [cldId, setCldId] = useState("");
+	console.log(cldId);
 	return (
-		<CldUploadWidget uploadPreset="xenyksnn">
-			{({ open }) => (
-				<button onClick={() => open()} className="btn btn-primary">
-					Upload
-				</button>
-			)}
-		</CldUploadWidget>
+		<>
+			{cldId && <CldImage src={cldId} alt="gol" width={270} height={150} />}
+			<CldUploadWidget
+				uploadPreset="xenyksnn"
+				onSuccess={(result, widget) => {
+					const results = result.info as CloudnaryProps;
+					setCldId(results.public_id);
+					widget.close();
+				}}
+			>
+				{({ open }) => (
+					<button onClick={() => open()} className="btn btn-primary">
+						Upload
+					</button>
+				)}
+			</CldUploadWidget>
+		</>
 	);
 }
