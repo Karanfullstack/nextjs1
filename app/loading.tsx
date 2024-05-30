5054c89c733b6aa0faa75822cc7869453fbffc7b
Karan Chauhan <karanchouhan211@gmail.com>
2024-05-30 22:20:56 +0200

    lazy loading dynamic imports optimization

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function loading() {
-	return <span className="loading loading-spinner loading-lg">My Loading...</span>;
+	return (
+		<span className="loading loading-spinner loading-lg">My Loading...</span>
+	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,27 @@
-import Link from "next/link";
-import ProductCard from "./components/Product/ProductCard";
-import { getServerSession } from "next-auth";
-import authOptions from "./api/auth/options";
-import Image from "next/image";
-import gol from "../assets/gol.jpeg";
-export default async function Home() {
-	const session = await getServerSession(authOptions);
+"use client";
+import dynamic from "next/dynamic";
+import { useState } from "react";
+const HeavyComponent = dynamic(() => import("./components/HeavyComponent"), {
+	ssr: false,
+	loading: () => <p>Loading..</p>,
+});
 
+export default async function Home() {
+	const [isVisible, setVisible] = useState(false);
 	return (
 		<main>
-			<h1>{session?.user!.name}</h1>
-			<Link href="/users">
-				<h1>Hello World</h1>
-			</Link>
-			<ProductCard />
-			<Image
-				src={
-					"https://m.media-amazon.com/images/S/pv-target-images/2f01f3ea957b378822c7aac932885fb1712799126fcd9bd192e74fc4d06dd545._AC_SX720_FMwebp_.jpg"
-				}
-				height={170}
-				width={250}
-				alt="gol"
-			/>
+			<button
+				onClick={async () => {
+					const _ = (await import("lodash")).default;
+					const users = [{ name: "c" }, { name: "b" }, { name: "a" }];
+					const result = _.orderBy(users, ["name"]);
+					console.log(result);
+				}}
+				className="btn btn-link"
+			>
+				Click
+			</button>
+			{isVisible && <HeavyComponent />}
 		</main>
 	);
 }
